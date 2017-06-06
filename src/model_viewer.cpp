11614e8819406a3128fba2208a811beefb8928fb
Snapple49 <oliver-dot-stein@hotmail.com>
2017-06-06 23:07:34 +0200

    added stroke color tweak in GUI

--- src/model_viewer.cpp
+++ src/model_viewer.cpp
@@ -22,6 +22,7 @@
 #include <algorithm>
 
 glm::vec3 shader_switch = glm::vec3(1.0, 1.0, 1.0);
+glm::vec3 stroke_color = glm::vec3(1.0);
 GLboolean bShowNormalsAsRgb = false;
 GLboolean bUseGammaCorrection = true;
 float zoomFactor = 1.0;
@@ -259,6 +260,8 @@ void drawMesh(Context &ctx, GLuint program, const MeshVAO &meshVAO)
 	glUniform3fv(glGetUniformLocation(ctx.program, "u_light_color"), 1, &light_color[0]);
 
 	glUniform3fv(glGetUniformLocation(ctx.program, "u_shader_switch"), 1, &shader_switch[0]);
+	glUniform3fv(glGetUniformLocation(ctx.program, "u_stroke_color"), 1, &stroke_color[0]);
+
 	if (bShowNormalsAsRgb) {
 		glUniform1f(glGetUniformLocation(ctx.program, "u_showNormalsAsRgb"), 1.0);
 	}
@@ -529,7 +532,13 @@ int main(void)
 		ImGui::NextColumn();
 		ImGui::Separator();
 
-		
+		//Stroke color
+		ImGui::Text("STROKE COLOR");
+		ImGui::SliderFloat("Red", &stroke_color.r, 0.0f, 1.0f, "%.2f");
+		ImGui::SliderFloat("Green", &stroke_color.g, 0.0f, 1.0f, "%.2f");
+		ImGui::SliderFloat("Blue", &stroke_color.b, 0.0f, 1.0f, "%.2f");
+		ImGui::NextColumn();
+		ImGui::Separator();
 
 		//ImGui_ImplGlfwGL3_NewFrame();
 		display(ctx);
